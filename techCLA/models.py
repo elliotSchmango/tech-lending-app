dff96c8567b4e69b0d07210e159f5765b4aa8575
kyle-luong <133829822+kyle-luong@users.noreply.github.com>
2025-02-18 13:49:07 -0500

    Added user model for patrons and librarians

--- techCLA/models.py
+++ techCLA/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('patron', 'Patron'),
+        ('librarian', 'Librarian'),
+    ]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default=PATRON)
+
+    def is_librarian(self) -> bool:
+        return self.role == self.LIBRARIAN
