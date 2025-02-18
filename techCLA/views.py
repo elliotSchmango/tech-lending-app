5f7589189de9a98b622be36ca7c4138676c749ec
kyle-luong <133829822+kyle-luong@users.noreply.github.com>
2025-02-18 14:46:55 -0500

    Integrated user roles with index, reorganized code

--- techCLA/views.py
+++ techCLA/views.py
@@ -4,6 +4,24 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("Hello, world. You're at the technology lending app index page.")
+    if request.user.is_authenticated:
+        username = request.user.get_full_name() or request.user.username
+        if request.user.role == 'librarian':
+            role = 'librarian'
+            welcome_message = f"Welcome, {username}! You have administrative privileges."
+        else:
+            role = 'patron'
+            welcome_message = f"Welcome, {username}! Enjoy browsing our collections."
+    else:
+        role = 'anonymous'
+        username = ''
+        welcome_message = "Welcome to our Catalog! Please log in to access all features."
+    
+    context = {
+        'welcome': welcome_message,
+        'username': username,
+        'role': role,
+    }
+    
+    return render(request, 'techCLA/index.html', context)
