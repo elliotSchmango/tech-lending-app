6625689af6f9616a07b36c7855154b93bcf84d39
Bowen Slingluff <bcslingluff@gmail.com>
2025-02-17 22:42:42 -0500

    updated urls for app to include oauth login page

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path,include
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('', include("techCLA.urls")),
     path('admin/', admin.site.urls),
+    path('',include('techCLA.urls')),
+
+    path('', TemplateView.as_view(template_name='index.html')),
+    path('accounts/', include('allauth.urls')),
+    path('logout', LogoutView.as_view())
 ]
