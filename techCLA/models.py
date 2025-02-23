92c9405144fbc832bc280bad033fc5432fe8578e
Bowen Slingluff <bcslingluff@gmail.com>
2025-02-23 17:42:11 -0500

    Now using groups to determine user's role. Login automatically assigns user to Patron

--- techCLA/models.py
+++ techCLA/models.py
@@ -2,14 +2,25 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    ROLE_CHOICES = [
-        ('patron', 'Patron'),
-        ('librarian', 'Librarian'),
-    ]
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='patron')
+
+    @property
+    def role(self):
+        """Returns the user's primary group as a role name."""
+        groups = self.groups.values_list('name', flat=True)
+        if "Librarian" in groups:
+            return "Librarian"
+        elif "Patron" in groups:
+            return "Patron"
+        elif "Admin" in groups:
+            return "Admin"
+        else:
+            return "Anonymous"
 
     def is_librarian(self):
-        return self.role == 'librarian'
+        return self.groups.filter(name='Librarian').exists()
+    
+    def is_patron(self):
+        return self.groups.filter(name='Patron').exists()
 
 class Collection(models.Model):
     VISIBILITY_CHOICES = [
