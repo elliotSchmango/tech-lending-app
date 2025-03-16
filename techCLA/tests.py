66d6c38cb4c0010be47d0e86b9b9be3e0fca2c94
Alex Song <asong@Alexs-MacBook-Air.local>
2025-03-16 00:09:04 -0400

    wrote UserModelTests.test_default_role

--- techCLA/tests.py
+++ techCLA/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import User
+
+
+class UserModelTests(TestCase):
+    def test_default_role(self):
+        patron = User.objects.create_user(username="testpatron")
+
+        self.assertIs(patron.is_librarian(), False)
+        self.assertIs(patron.is_patron(), True)
+
+
