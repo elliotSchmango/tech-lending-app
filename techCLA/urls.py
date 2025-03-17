c3eae397e02dd0176af26a800c0b3d4d59cecaf4
allenh99 <ahthing99@gmail.com>
2025-03-16 21:19:41 -0400

    working profile pictures update at /profile/update-profile

--- techCLA/urls.py
+++ techCLA/urls.py
@@ -3,11 +3,12 @@ from django.contrib.auth import views as auth_views
 from allauth.account import views as allauth_views
 
 from . import views
-from .views import borrow_item
+from .views import borrow_item,update_profile
 
 urlpatterns = [
     path("", views.index, name="index"),
     path('catalog/<str:item_name>/borrow/', borrow_item, name='borrow_item'),
-    path("catalog/", views.CatalogView.as_view(), name="catalog")
+    path("catalog/", views.CatalogView.as_view(), name="catalog"),
+    path("profile/update-profile/", update_profile, name="update_profile"),
 ]
 
