a2aa1915ecd4801b0a10924369cbbe7791701329
Kyle <kyleluong2004@gmail.com>
2025-04-26 14:55:07 -0400

    navbar+sidebar revamp, index modifications

--- techCLA/context_processors.py
+++ techCLA/context_processors.py
@@ -1,18 +1,21 @@
 from .models import Collection
 
 def navbar_collections(request):
-    if request.user.is_authenticated:
-        if request.user.role == 'Librarian':
-            collections = Collection.objects.all()
-        else:
-            collections = Collection.objects.filter(visibility='public')
-    else:
-        collections = Collection.objects.filter(visibility='public')
+    public_collections = Collection.objects.filter(visibility='public')
 
-    main_collections = collections[:5]
-    other_collections = collections[5:]
+    # first 5 public collections for the navbar
+    main_collections = public_collections[:5]
+    other_collections = public_collections[5:]
+
+    # priv collections if logged in
+    if request.user.is_authenticated:
+        private_collections = Collection.objects.filter(visibility='private')
+    else:
+        private_collections = None
 
     return {
-        'collections': main_collections,
+        'collections': main_collections,  
         'other_collections': other_collections,
+        'public_collections': public_collections,
+        'private_collections': private_collections,   
     }
